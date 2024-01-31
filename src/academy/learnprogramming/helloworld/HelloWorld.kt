868096e8045ce862e71c650b8e498b16c190a1cb
keyhong <wnhong96@gmail.com>
2024-01-31 22:51:51 +0900

    kotlin first commit

---
+++ src/academy/learnprogramming/helloworld/HelloWorld.kt
@@ -0,0 +1,26 @@
+package academy.learnprogramming.helloworld
+
+fun main(args: Array<String>) {
+    var number: Int
+    number = 10
+    number = 20
+
+    val employee1 = Employee("Lynn Jones", 500)
+    employee1.name = "Lynn Smith"
+
+    val employee2: Employee
+    val number2 = 100
+
+    if (number < number2) {
+        employee2 = Employee("Jane Smith", 400)
+    } else {
+        employee2 = Employee("Mike", 150)
+    }
+
+
+
+}
+
+class Employee(var name: String, val id: Int) {
+}
+
